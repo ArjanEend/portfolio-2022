5f217fb3a5cb53957f26eba321ef265c8bfa0010
Arjan Cordia <arjan@lucky-kat.com>
2022-06-06 11:06:44 +0200

    ADD about

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import Navbar, { HeaderLink } from './components/Navbar';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Routing, { AppRoute } from './components/Routing';
+import About from './components/About';
 
 const routes: Array<HeaderLink & AppRoute> = [
   { path: "/", label: "Home", element: <Home /> },
-  { path: "/", label: "Home", element: <Home /> },
+  { path: "/", label: "About", element: <About /> },
   { path: "/", label: "Home", element: <Home /> },
 ]
 
